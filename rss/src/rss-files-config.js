6eea01b3e113a8d00843ed7502b5e59e02b2e4b4
Bela Olson <bela.olson@nih.gov>
2023-05-30 13:32:28 -0400

    (#12) Update Link Locations
    - Updated link locations in rss-files-config.js
    - XML file names are lowercase and use underscores
    
    Closes #12

--- rss/src/rss-files-config.js
+++ rss/src/rss-files-config.js
@@ -2,44 +2,50 @@ const rss = [
 	{
 		title: 'Medical Devices',
 		description: 'Available Technologies from the Technology Transfer Center.',
-		link: 'http://www.example.com',
 		filters:
 			'field_govdelivery:false AND field_ics:NCI AND (field_applications:"Medical Devices" OR field_applications:"Non-Medical Devices")',
+		link: 'https://techtransfer.cancer.gov/rss/medical_devices.xml',
+		xmlFilename: 'medical_devices',
 	},
 	{
 		title: 'Diagnostics',
 		description: 'Available Technologies from the Technology Transfer Center.',
-		link: 'http://www.example.com',
 		filters:
 			'field_govdelivery:false AND field_ics:NCI AND (field_applications:Diagnostics)',
+		link: 'https://techtransfer.cancer.gov/rss/diagnostics.xml',
+		xmlFilename: 'diagnostics',
 	},
 	{
 		title: 'Research Materials',
 		description: 'Available Technologies from the Technology Transfer Center.',
-		link: 'http://www.example.com',
 		filters:
 			'field_govdelivery:false AND field_ics:NCI AND (field_applications:"Research Materials")',
+		link: 'https://techtransfer.cancer.gov/rss/research_materials.xml',
+		xmlFilename: 'research_materials',
 	},
 	{
 		title: 'Therapeutics',
 		description: 'Available Technologies from the Technology Transfer Center.',
-		link: 'http://www.example.com',
 		filters:
 			'field_govdelivery:false AND field_ics:NCI AND (field_applications:Therapeutics)',
+		link: 'https://techtransfer.cancer.gov/rss/therapeutics.xml',
+		xmlFilename: 'therapeutics',
 	},
 	{
 		title: 'Software and Apps',
 		description: 'Available Technologies from the Technology Transfer Center.',
-		link: 'http://www.example.com',
 		filters:
 			'field_govdelivery:false AND field_ics:NCI AND (field_applications:"Software / Apps")',
+		link: 'https://techtransfer.cancer.gov/rss/software_and_apps.xml',
+		xmlFilename: 'software_and_apps',
 	},
 	{
 		title: 'Vaccines',
 		description: 'Available Technologies from the Technology Transfer Center.',
-		link: 'http://www.example.com',
 		filters:
 			'field_govdelivery:false AND field_ics:NCI AND (field_applications:Vaccines)',
+		link: 'https://techtransfer.cancer.gov/rss/vaccines.xml',
+		xmlFilename: 'vaccines',
 	},
 ];
 
